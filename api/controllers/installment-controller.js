a985ff4789712ed47369ebe338f5ae33fa934430
Nizam <nizam@Nizams-MacBook-Pro.local>
2022-04-29 14:07:54 +0600

    Changed API

--- api/controllers/installment-controller.js
+++ api/controllers/installment-controller.js
@@ -1,24 +1,22 @@
 const mongoose = require("mongoose");
 
 const {
-  getAllBranches,
   getTotalMatch,
   findById,
-  doesItExist,
-  deleteBranch,
-  updateOneBranch,
   insertMany,
+  getAllInstallments,
 } = require("../services/installment-service");
 
 const index = async (req, res) => {
   try {
     const limit = parseInt(req.query.limit, 10) || 100;
     const skip = parseInt(req.query.offset, 10) || 0;
+
     const query = req.query.search || "";
     const totalMatch = await getTotalMatch(query);
-    const branches = await getAllBranches(query, skip, limit);
+    const installments = await getAllInstallments(query, skip, limit);
     return res.status(200).json({
-      results: branches,
+      results: installments,
       total: totalMatch[0] && totalMatch[0].total ? totalMatch[0].total : 0,
     });
   } catch (e) {
@@ -72,54 +70,10 @@ const store = async (req, res) => {
   });
 };
 
-const updateOne = async (req, res) => {
-  const data = {
-    name: req.body.name,
-  };
-  const { id } = req.params;
-
-  try {
-    const doesBranchExist = await doesItExist(req, 1);
-    if (doesBranchExist) {
-      return res.status(409).json({
-        message: "Branch already exist",
-      });
-    }
-    await updateOneBranch(id, data);
-    const branch = await findById(id);
-    return res.status(200).json({
-      results: branch,
-      message: "Successfully updated branch",
-    });
-  } catch (e) {
-    if (e.message.indexOf("duplicate key error") !== -1)
-      return res.status(409).json({
-        message: "Validation failed.",
-        errors: [{ name: `"${data.name}" Already exist.` }],
-      });
-    const errors = [];
-    if (e.errors) {
-      const keys = Object.keys(e.errors || {});
-      for (let i = 0; i < keys.length; i += 1) {
-        errors.push({ [keys[i]]: e.errors[keys[i]].message });
-      }
-    }
-    if (errors.length)
-      return res.status(400).json({
-        message: "Validation failed.",
-        errors,
-      });
-
-    return res.status(500).json({
-      message: e.message,
-    });
-  }
-};
-
 const deleteOne = async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await deleteBranch(id);
+    const result = await deleteOne(id);
     if (result.deletedCount === 0)
       return res.status(200).json({
         message: "Record could not found.",
@@ -150,4 +104,4 @@ const findOne = async (req, res) => {
   }
 };
 
-module.exports = { index, store, updateOne, deleteOne, findOne };
+module.exports = { index, store, deleteOne, findOne };
--- api/routes/installment.js
+++ api/routes/installment.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const {
   index,
   store,
-  updateOne,
   deleteOne,
   findOne,
 } = require("../controllers/installment-controller");
@@ -14,8 +13,6 @@ router.get("/", index);
 router.post("/", store);
 
 router.get("/:id", findOne);
-
-router.patch("/:id", updateOne);
 
 router.delete("/:id", deleteOne);
 
