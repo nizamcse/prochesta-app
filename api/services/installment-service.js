1b66160dff8106314515698e4ec89a3f41cf75fb
Nizam <nizam@Nizams-MacBook-Pro.local>
2022-04-30 12:35:37 +0600

    KK

--- api/services/installment-service.js
+++ api/services/installment-service.js
@@ -1,7 +1,80 @@
 const Installment = require("../models/installment-collection");
 
 const getAllInstallments = (q, s, l) =>
-  Installment.aggregate([{ $skip: s }, { $limit: l }]);
+  Installment.aggregate([
+    {
+      $lookup: {
+        from: "loans",
+        localField: "loan",
+        foreignField: "_id",
+        as: "loan",
+      },
+    },
+    {
+      $unwind: {
+        path: "$loan",
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+    {
+      $lookup: {
+        from: "clients",
+        localField: "loan.client",
+        foreignField: "_id",
+        as: "loan.client",
+      },
+    },
+    {
+      $unwind: {
+        path: "$loan.client",
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+    {
+      $lookup: {
+        from: "centers",
+        localField: "loan.client.center",
+        foreignField: "_id",
+        as: "loan.client.center",
+      },
+    },
+    {
+      $unwind: {
+        path: "$loan.client.center",
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+    {
+      $lookup: {
+        from: "branches",
+        localField: "loan.client.center.branch",
+        foreignField: "_id",
+        as: "loan.client.center.branch",
+      },
+    },
+    {
+      $unwind: {
+        path: "$loan.client.center.branch",
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+    {
+      $lookup: {
+        from: "branches",
+        localField: "loan.client.branch",
+        foreignField: "_id",
+        as: "loan.client.branch",
+      },
+    },
+    {
+      $unwind: {
+        path: "$loan.client.branch",
+        preserveNullAndEmptyArrays: true,
+      },
+    },
+    { $skip: s },
+    { $limit: l },
+  ]);
 // eslint-disable-next-line no-unused-vars
 const getTotalMatch = (_q) => Installment.aggregate([{ $count: "total" }]);
 
